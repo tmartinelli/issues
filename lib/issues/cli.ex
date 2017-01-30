7a1658a9c0e5a09ee37b100517f91be789a73863
tmartinelli <thiagov.martinelli@gmail.com>
2017-01-30 21:25:56 -0200

    add process method to cli module

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -9,7 +9,18 @@ defmodule Issues.CLI do
     """
 
     def run(argv) do
-      parse_args(argv)
+      argv
+      |> parse_args
+      |> process
+    end
+
+    def process(:help) do
+      IO.puts("usage: issues <user> <project> [count | #{@default_count}]")
+      System.halt(0)
+    end
+
+    def process({ user, project, _count }) do
+      Issues.GitHubIssues.fetch(user, project)
     end
 
     @doc """
